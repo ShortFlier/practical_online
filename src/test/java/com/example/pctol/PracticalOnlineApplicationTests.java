ff05f29252f7951901e1a889111d406a15e235ce
liufw <3280207240@qq.com>
2024-03-23 23:21:09 +0800

    新建项目

---
+++ src/test/java/com/example/pctol/PracticalOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pctol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
