6082e6e1887e0d270b058cd81abedecb097493dd
liufw <3280207240@qq.com>
2024-03-25 16:13:59 +0800

    简单全局异常处理

---
+++ src/main/java/com/example/pctol/server/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.pctol.server.exception;
+
+
+import com.example.pctol.common.constant.StateCode;
+import com.example.pctol.pojo.VO.Result;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public Result ex(Exception ex){
+        Throwable lastException = ex;
+        while (lastException.getCause() != null) {
+            lastException = lastException.getCause();
+        }
+        return new Result(StateCode.FAILED, lastException.getMessage());
+    }
+}
